67e5d621e980a502e7d0bb104fcb54e9a9998797
x0newt <qwerty0w0@protonmail.com>
2022-02-07 18:18:51 +0000

    formatting

--- memoir.c
+++ memoir.c
@@ -1,10 +1,6 @@
 // TODO:
 // add $home etc and same for windows so it works for all systems - enviroment variables
 // look into encryption of password and entries.
-// signature: this is the file that you want to encrypt
-// encrypted: the file which encrypted from "signature"
-// decrypted: the file which decrypted from "encrypted", it should be the same as "signature"
-
 
 #include <string.h>
 #include <unistd.h>
@@ -18,21 +14,20 @@
 #include <openssl/aes.h>
 
 int main() {
-  const char* documents = "documents";
-  const char* append = "append";
-  const char* create = "create";
-  const char* view = "view";
-  const char* help = "help";
-  const char* y = "y";
-  const char* n = "n";
-  const char* fin = "fin";
+  const char * documents = "documents";
+  const char * append = "append";
+  const char * create = "create";
+  const char * view = "view";
+  const char * help = "help";
+  const char * y = "y";
+  const char * n = "n";
+  const char * fin = "fin";
 
   unsigned char indata[AES_BLOCK_SIZE];
   unsigned char outdata[AES_BLOCK_SIZE];
   unsigned char decryptdata[AES_BLOCK_SIZE];
   unsigned char userkey[] = "\x09\x8F\x6B\xCD\x46\x21\xD3\x73\xCA\xDE\x4E\x83\x26\x27\xB4\xF6";
   unsigned char IV[] = "\x0A\x91\x72\x71\x6A\xE6\x42\x84\x09\x88\x5B\x8B\x82\x9C\xCB\x05";
-
 
   char entry[150];
   char filename[65];
@@ -41,32 +36,31 @@ int main() {
   char passpath[PATH_MAX];
   char docent[PATH_MAX];
   char doc[PATH_MAX];
+  char encpass[PATH_MAX];
 
-  char* appendstr;
-  char* diarystr;
-  char* setpass;
-  char* reenter;
-  char* diary;
-  char* passcheck;
+  char * appendstr;
+  char * diarystr;
+  char * setpass;
+  char * reenter;
+  char * diary;
+  char * passcheck;
 
   appendstr = malloc(sizeof(char) * 1024);
   diarystr = malloc(sizeof(char) * 1024);
   diary = malloc(sizeof(char) * 1024);
 
-  struct tm* timenow;
+  struct tm * timenow;
   time_t now = time(NULL);
   timenow = gmtime( & now);
 
   AES_KEY key;
 
-  FILE* fp;
-  FILE* pc;
-  DIR* d;
+  FILE * fp;
+  FILE * pc;
+  DIR * d;
 
-  strcat(strcpy(encpass, getenv("HOME")),".config/memoirpass/passenc.txt");
-  strcat(strcpy(docent, getenv("HOME")), "/Documents/Mementries");
-  strcat(strcpy(passtxt, getenv("HOME")), "/.config/memoirpass/pass.txt");
   strcat(strcpy(passpath, getenv("HOME")), "/.config/memoirpass/");
+  strcat(strcpy(docent, getenv("HOME")), "/Documents/Mementries");
 
   void timestamp() {
     strcat(strcpy(doc, getenv("HOME")), "/Documents/Mementries/%d-%m-%Y.txt");
@@ -76,7 +70,7 @@ int main() {
 
   void landingmsg() {
     printf("Would you like to create an entry or append an existing entry?\n");
-    printf("Please choose create or append, use help to list commands.\n");
+    printf("Please choose create or append, use help to list additional commands.\n");
   }
 
   void screenwipe() {
@@ -110,8 +104,8 @@ int main() {
         scanf(" %[^\n]s*c", diarystr);
         fprintf(fp, "%s%s %s\n", currenttime, appendstr, diarystr);
         fclose(fp);
-        }
-     }
+      }
+    }
   }
 
   void promptcre() {
@@ -128,54 +122,54 @@ int main() {
         scanf(" %[^\n]s*c", diarystr);
         fprintf(fp, "%s%s %s", currenttime, diary, diarystr);
         fclose(fp);
-        }
+      }
+    }
   }
-}
 
-  void(encrypt){
-    FILE *ifp, *ofp;
+  void encrypt() {
+    FILE * ifp, * ofp;
     ifp = fopen(encpass, "r+");
     ofp = fopen(passtxt, "w+");
     int postion = 0;
     int bytes_read, bytes_write;
-    while(1){
+    while (1) {
       unsigned char ivec[AES_BLOCK_SIZE];
       memcpy(ivec, IV, AES_BLOCK_SIZE);
       bytes_read = fread(indata, 1, AES_BLOCK_SIZE, ifp);
-      AES_cfb128_encrypt(indata, outdata, bytes_read, &key, ivec, &postion, AES_ENCRYPT);
+      AES_cfb128_encrypt(indata, outdata, bytes_read, & key, ivec, & postion, AES_ENCRYPT);
       bytes_write = fwrite(outdata, 1, bytes_read, ofp);
       if (bytes_read < AES_BLOCK_SIZE)
         break;
-      }
-      fclose(ifp);
-      fclose(ofp);
-     }
-
-void decrypt(){
-  FILE *ifp, *ofp;
-  ifp = fopen(passtxt, "r+");
-  ofp = fopen(encpass, "w+");
-  int postion = 0;
-  int bytes_read, bytes_write;
-  while(1){
-    unsigned char ivec[AES_BLOCK_SIZE];
-    memcpy(ivec, IV, AES_BLOCK_SIZE);
-    bytes_read = fread(outdata, 1, AES_BLOCK_SIZE, ifp);
-    AES_cfb128_encrypt(outdata, decryptdata, bytes_read, &key, ivec, &postion, AES_DECRYPT);
-    bytes_write = fwrite(decryptdata, 1, bytes_read, ofp);
-    if (bytes_read < AES_BLOCK_SIZE)
-      break;
     }
     fclose(ifp);
     fclose(ofp);
-   }
+  }
+
+  void decrypt() {
+    FILE * ifp, * ofp;
+    ifp = fopen(passtxt, "r+");
+    ofp = fopen(encpass, "w+");
+    int postion = 0;
+    int bytes_read, bytes_write;
+    while (1) {
+      unsigned char ivec[AES_BLOCK_SIZE];
+      memcpy(ivec, IV, AES_BLOCK_SIZE);
+      bytes_read = fread(outdata, 1, AES_BLOCK_SIZE, ifp);
+      AES_cfb128_encrypt(outdata, decryptdata, bytes_read, & key, ivec, & postion, AES_DECRYPT);
+      bytes_write = fwrite(decryptdata, 1, bytes_read, ofp);
+      if (bytes_read < AES_BLOCK_SIZE)
+        break;
+    }
+    fclose(ifp);
+    fclose(ofp);
+  }
 
   if (access(passtxt, F_OK) == 0) {
     while (1) {
       FILE * f = fopen(passtxt, "r");
       fseek(f, 0, SEEK_END);
       long fsize = ftell(f);
-      fseek(f, 0, SEEK_SET); 
+      fseek(f, 0, SEEK_SET);
 
       char * pass = malloc(fsize + 1);
       fread(pass, fsize, 1, f);
@@ -190,7 +184,7 @@ void decrypt(){
         landingmsg();
         while (1) {
           scanf("%s", entry);
-            if (strcmp(append, entry) == 0) {
+          if (strcmp(append, entry) == 0) {
             timestamp();
             fp = fopen(filename, "a");
             screenwipe();
@@ -203,7 +197,7 @@ void decrypt(){
             timestamp();
             fp = fopen(filename, "w"); // creates file
             screenwipe(); // wipes  cmd again for diary entry
-            printf("Begin diary entry:\nPress enter to confirm your diary entry.\n"); 
+            printf("Begin diary entry:\nPress enter to confirm your diary entry.\n");
             scanf(" %[^\n]s", diary); // takes diary entry 
             fprintf(fp, "%s%s\n", currenttime, diary);
             fclose(fp); // close the file after entry is done
@@ -251,6 +245,7 @@ void decrypt(){
     pc = fopen(passtxt, "w");
     fprintf(pc, "%s", setpass);
     fclose(pc);
+    encrypt();
     printf("%s will now be set as your password on next launch.\n\n", setpass);
     printf("It seems you also don't have a folder directory set for your diary entries.\n"
       "Would you like to set it on your own or have it in your docuemnts folder?\n"
@@ -262,7 +257,7 @@ void decrypt(){
         printf("Chosen directory created.");
       } else {
         chdir(getenv("HOME"));
-        printf("Current working directory: %s\n",getenv("HOME"));
+        printf("Current working directory: %s\n", getenv("HOME"));
         scanf("%s", entry);
         mkdir(entry, 0700);
         printf("Chosen directory created.");
